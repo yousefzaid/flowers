72d956571c755c86821912d2fc2e410cd0978b0f
alasidig <alasidig@users.noreply.github.com>
2021-10-31 14:43:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -49,19 +49,20 @@
 </nav>
 
 <div class="row">
-<div class="col-md-2 col-lg-4">
+<div class="col-md-6 col-lg-4">
 <div class="card">
   <img src="https://services.hanselandpetal.com/photos/california_snow.jpg" class="card-img-top" alt="california_snow.jpg">
   <div class="card-body">
     <h5 class="card-title">Shrubs</h5>
-    <p class="card-text">price: 15.99,</p>
-    <a href="detail.php" class="btn btn--outline-info">More details</a>
+    <h5 class="card-subtitle">price: 15.99,</h5>
+    
+    <a href="detail.php" class="btn btn-outline-info">More details</a>
   </div>
 </div>
 </div>
 </div>
 
-footer class="bg-primary">
+<footer class="bg-primary">
   <div class="container text-white">
     <div class="row">
       <div class="col-md-4">
