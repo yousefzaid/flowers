d04957e2c4c1f5efaef0a24dc53c2ef3fd66d10e
alasidig <alasidig@users.noreply.github.com>
2021-10-31 14:12:41 +0300

    Create detail.php

---
+++ detail.php
@@ -0,0 +1,12 @@
+<div class="row">
+<div class="col-md-2 col-lg-4">
+<div class="card">
+  <img src="https://services.hanselandpetal.com/photos/california_snow.jpg" class="card-img-top" alt="california_snow.jpg">
+  <div class="card-body">
+    <h5 class="card-title">Shrubs</h5>
+    <p class="card-text">price: 15.99,</p>
+    <a href="detail.php" class="btn btn--outline-info">More details</a>
+  </div>
+</div>
+</div>
+</div>
