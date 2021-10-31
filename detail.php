0968fa65c2934006146716034de3d4005eb8f9e0
alasidig <alasidig@users.noreply.github.com>
2021-10-31 14:44:01 +0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,11 +1,13 @@
 <div class="row">
-<div class="col-md-2 col-lg-4">
+<div class="col-md-6 col-lg-4">
 <div class="card">
   <img src="https://services.hanselandpetal.com/photos/california_snow.jpg" class="card-img-top" alt="california_snow.jpg">
   <div class="card-body">
     <h5 class="card-title">Shrubs</h5>
-    <p class="card-text">price: 15.99,</p>
-    <a href="detail.php" class="btn btn--outline-info">More details</a>
+    <h5 class="card-subtitle">price: 15.99,</h5>
+    <p class="card-text">"instructions": "Large double. Good grower, heavy bloomer. Early to mid-season, acid loving plants. Plant in moist well drained soil with pH of 4.0-5.5.",
+      </p>
+    
   </div>
 </div>
 </div>
